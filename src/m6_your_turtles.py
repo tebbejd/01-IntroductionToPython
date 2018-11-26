814a8fea6c3e24ddda264de01473e75e11fb8661
Jacob Tebbe <tebbejd@rose-hulman.edu>
2018-11-26 15:10:46 -0500

    Class 1_DONE

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Jacob Tebbe.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,36 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+fred = rg.SimpleTurtle('turtle')
+fred.pen = rg.Pen('green', 2)
+fred.speed = 10
+
+for k in range(15):
+    fred.draw_circle(k * 10)
+    fred.pen_up()
+    fred.right(90)
+    fred.forward(k * 10)
+    fred.pen_down()
+
+george = rg.SimpleTurtle('turtle')
+george.pen = rg.Pen('red', 3)
+george.speed = 5
+
+george.pen_up()
+george.go_to(rg.Point(275, 175))
+george.pen_down()
+
+window.tracer(25)
+
+for k in range(20):
+    george.draw_regular_polygon(5,55)
+    george.pen_up()
+    george.left(45)
+    george.forward(10)
+    george.pen_down()
+
+window.close_on_mouse_click()
